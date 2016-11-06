058868c50cccc1878c386007ea6308590c5d911e
Ankit Chaudhary <my007id@gmail.com>
2016-11-06 16:51:17 +0100

    added implementation for finder

--- src/main/java/com/goeuro/routefinder/component/PlannedRoutes.java
+++ src/main/java/com/goeuro/routefinder/component/PlannedRoutes.java
@@ -5,11 +5,18 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 @Component
 public class PlannedRoutes implements ApplicationRunner {
 
+    public static List<String> listOfStops;
     private String filePath;
 
     /**
@@ -22,5 +29,30 @@ public class PlannedRoutes implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         List<String> value = args.getOptionValues("some");
         filePath = value.get(0);
+        listOfStops = createBusRouteFromFile();
     }
+
+
+    public List<String> createBusRouteFromFile() {
+        List<String> list = new ArrayList<>();
+        try {
+            Scanner fileIn = new Scanner(new File(this.filePath));
+            int numberOfRoutes = Integer.parseInt(fileIn.nextLine());
+            while (fileIn.hasNext()) {
+                String inputLine = fileIn.nextLine().trim();
+                String routeFromLine = inputLine.substring(inputLine.indexOf(" ")).concat(" ");
+                list.add(routeFromLine);
+            }
+            if (list.size() == numberOfRoutes) {
+                return list;
+            } else {
+                return Collections.emptyList();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+
 }
