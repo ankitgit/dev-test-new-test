7a9e1425ab655df38f5b8997be08ab1ea26bba7e
Ankit Chaudhary <my007id@gmail.com>
2016-11-06 14:34:00 +0100

    controll class

---
+++ src/main/java/com/goeuro/routefinder/controller/PathFinder.java
@@ -0,0 +1,9 @@
+package com.goeuro.routefinder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/find")
+public class PathFinder {
+}
