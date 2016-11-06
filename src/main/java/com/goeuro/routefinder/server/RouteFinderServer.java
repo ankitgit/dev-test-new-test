9c35eddb47401f0eb82c8eb33972beb6cecb00e2
Ankit Chaudhary <my007id@gmail.com>
2016-11-06 14:22:08 +0100

    added server code

---
+++ src/main/java/com/goeuro/routefinder/server/RouteFinderServer.java
@@ -0,0 +1,12 @@
+package com.goeuro.routefinder.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteFinderServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteFinderServer.class, args);
+    }
+}
