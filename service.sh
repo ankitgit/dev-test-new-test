c6a212bef113579ce03a398187f024329d72f3c5
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-05-22 11:03:08 +0200

    #14: fixed service.sh

--- service.sh
+++ service.sh
@@ -6,9 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RUN="java -jar $DIR/build/libs/go-euro-route-finder-service-0.0.1.jar --fileloc=$2"
 NAME=go-euro-route-finder
 
-
-
-
 DATA_FILE=$2
 
 PIDFILE=/tmp/$NAME.pid
